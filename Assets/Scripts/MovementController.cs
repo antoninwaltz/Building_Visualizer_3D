892eaea501bf6481bacdb92eb6d0a21edf0095a2
Johann NOVAK <johann.novak25@gmail.com>
2015-10-29 15:34:50 +0600

    unity project commited

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour {
+
+	CharacterController m_controller;
+
+	public float m_movementSpeed;
+	private Vector3 m_movementDirection;
+
+	public float m_horizontalAngleSpeed;
+	public float m_verticalAngleSpeed;
+	public float m_maxYAngle;
+	public float m_minYAngle;
+
+	// Use this for initialization
+	void Start () {
+		m_controller = GetComponent<CharacterController> ();
+		m_movementDirection = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
